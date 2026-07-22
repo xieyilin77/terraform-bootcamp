b2018b2c1f758cc67e6ae446985e13b7d9ed662f
xieyilin77 <yilin-x@hotmail.de>
2026-07-22 20:12:16 +0200

    add excise terraform backend

---
+++ terraform-backend-setup/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "terraform-state-yilin"
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock-yilin"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+    tags = {
+    Name = "terraform-lock-yilin"
+  }
+}
